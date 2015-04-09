e80f749c21e3521a1e44358e298df28c478c29f9
Gizem K <gzmk@users.noreply.github.com>
2015-04-09 14:50:19 -0400

    Update loop_fitbrdf.m

--- loop_fitbrdf.m
+++ loop_fitbrdf.m
@@ -34,8 +34,8 @@ for i = 1:iter
     setGlobalrod(XBest2(2))
     bestRhos = [bestRhos;XBest2];
     bestfit_2pr = [bestfit_2pr;BestF2];
-    delta1 = bestRhos(iter,1)*0.2;
-    delta2 = bestRhos(iter,2)*0.2;
+    delta1 = bestRhos(iter,1)*0.01;
+    delta2 = bestRhos(iter,2)*0.01;
     converge1 = abs(bestRhos(iter,1)-bestRhos(iter-1,1));
     converge2 = abs(bestRhos(iter,2)-bestRhos(iter-1,2));
     
@@ -48,7 +48,7 @@ for i = 1:iter
     setGlobalalpha(XBest1(1))
     bestAlphas = [bestAlphas;XBest1];
     bestfit_1pr = [bestfit_1pr;BestF1];
-    delta3 = bestAlphas(iter,1)*0.2;
+    delta3 = bestAlphas(iter,1)*0.01;
     converge3 = abs(bestAlphas(iter,1)-bestAlphas(iter-1,1));
     
     sprintf('Fix alphau: %f and fit rho_s and rho_d', XBest1);
